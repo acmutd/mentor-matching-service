77d34c46eca6bd5c78f70f19c1145d1b05964236
Willie Chalmers III <willie.chalmers@acmutd.co>
2021-09-12 15:36:05 -0500

    Move hard-coded Firebase values to eenv variables

--- matchingservice/frontend/src/firebase.js
+++ matchingservice/frontend/src/firebase.js
@@ -1,16 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 var firebaseConfig = {
-    apiKey: "AIzaSyD-pe9M4226Ai85ELkBejEVIVqqhRIvKqk",
-    authDomain: "fir-form-c0dbc.firebaseapp.com",
-    projectId: "fir-form-c0dbc",
-    storageBucket: "fir-form-c0dbc.appspot.com",
-    messagingSenderId: "447753363504",
-    appId: "1:447753363504:web:c153ed4827457135c00df5"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_APP_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  var db = firebase.firestore();
+var db = firebase.firestore();
 
-  export { db };
+export { db };
