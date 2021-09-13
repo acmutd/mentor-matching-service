1475d5c14648c5bf12e460a0ccb53c30688537ac
ashwin <ashwin.som2001@gmail.com>
2021-09-13 17:36:03 -0500

    Remove env variable PORT

--- matchingservice/backend/flaskapi.py
+++ matchingservice/backend/flaskapi.py
@@ -198,5 +198,5 @@ def uploadEmailsToDatabase():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORTNUM", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(8080)
     CORS(app)
