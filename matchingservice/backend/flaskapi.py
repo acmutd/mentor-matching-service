5754be278dedc116f2975366c7438588dd48cd6c
ashwin <ashwin.som2001@gmail.com>
2021-09-13 19:28:26 -0500

    Add ) to line 201

--- matchingservice/backend/flaskapi.py
+++ matchingservice/backend/flaskapi.py
@@ -198,5 +198,5 @@ def uploadEmailsToDatabase():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(8080)
+    app.run(debug=True, host="0.0.0.0", port=int(8080))
     CORS(app)
